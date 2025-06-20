5746082177c683eb65275fbbee7f3f8e95977a45
Tushar Kumar <tkgoyal104@gmail.com>
2025-06-20 19:33:54 +0530

    Initial commit - JavaFX + Gradle setup

---
+++ README.md
@@ -0,0 +1,55 @@
+DMRC Clone - Metro Ticket Machine Simulator
+
+A simulation of a Delhi Metro Rail Corporation (DMRC) Ticket Machine, built using Java, JavaFX, and QR technology.
+
+Features
+
+-Buy Metro Tickets
+-QR Code Ticket Generation (ZXing)
+-Recharge Smart Cards
+-JavaFX GUI 
+-Built with Gradle
+
+
+Tech Stack
+
+|  Tool         | Usage                        |
+|---------------|------------------------------|
+| Java 17+      | Core application logic       |
+| JavaFX 21     | GUI for the ticket machine   |
+| ZXing         | QR code generation           |
+| Gradle        | Project and dependency build |
+| IntelliJ IDEA | Development Environment      |
+
+
+Project Structure
+dmrc_clone/
+├── build.gradle
+├── README.md
+├── .gitignore
+└── src/
+└── main/
+├── java/
+│ └── com.dmrc/
+│ └── MainApp.java
+└── resources/
+
+Planned Screens
+
+-  Home Screen (Buy Ticket, Recharge, View Ticket)
+-  Station Selector with Fare Calculation
+-  QR Ticket Display & Save
+-  Card Recharge with Mock Balance
+
+---
+
+Author
+
+Tushar Kumar  
+[https://github.com/TusharKcode]
+
+---
+
+License
+
+This project is for educational/demo purposes and not affiliated with Delhi Metro Rail Corporation.
--- src/main/java/com/dmrc/Main.java
+++ src/main/java/com/dmrc/Main.java
@@ -1,1 +1,66 @@
 package com.dmrc;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import java.awt.*;
+
+public class Main extends Application{
+
+    @Override
+    public void start(Stage primaryStage){
+                                                                                //Title Part of the Screen
+        Label title = new Label("Welcome to DMRC Ticket Machine.");
+        title.setStyle("-fx-font-size : 20px, -fx-font-weight : bold");
+
+                                                                                //Buttons
+        Button buyTicketBtn = new Button("Buy Ticket");
+        Button rechargeCardBtn = new Button("Recharge Card");
+        Button viewTicketBtn = new Button("View Ticket");
+        Button exitBtn = new Button("Exit");
+
+                                                                                //Styling Buttons
+        buyTicketBtn.setMinWidth(200);
+        rechargeCardBtn.setMinWidth(200);
+        viewTicketBtn.setMinWidth(200);
+        exitBtn.setMinWidth(200);
+                                                                                //Buttons actions
+        buyTicketBtn.setOnAction(e -> System.out.println("Buy Ticket screen will open!"));
+        rechargeCardBtn.setOnAction(e -> System.out.println("Recharge Card screen will open!"));
+        viewTicketBtn.setOnAction(e -> System.out.println("Ticket info screen will open!"));
+        exitBtn.setOnAction(e -> primaryStage.close());
+
+                                                                                //Layout
+        VBox layout = new VBox(20);
+        layout.setPadding(new Insets(30, 20,30,20));
+        layout.setAlignment(Pos.CENTER);
+        layout.getChildren().addAll(title, buyTicketBtn, rechargeCardBtn, viewTicketBtn, exitBtn);
+
+                                                                                //Scene
+        Scene scene = new Scene(layout, 400, 400);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("DMRC Ticket Machine");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
